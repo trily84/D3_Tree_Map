1481183edefa87ba126b6d706884496d65ff4965
trily84 <tmly283@gmail.com>
2021-03-29 15:02:30 -0700

    first commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# D3_Heat_Map
-
-Heat Map Visualization using D3 library
-
-https://trily84.github.io/D3_Heat_Map/
--- index.js
+++ index.js
@@ -1,129 +1,156 @@
-// fetch data from json file
-fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
-	.then(response => response.json())
-	.then(response => {
-        const baseTemp = response.baseTemperature
-        const data = response.monthlyVariance
-        createBar(data, baseTemp)
-    })
+let countyURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+let educationURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
 
+let countyData
+let educationData
 
-function createBar(data, baseTemp) { 
+colorData = ["#053959", "#164c6e", "#356685", "#57819c", "#7b9aad", "#9cafba", "#a9bcc7"]
 
-    // create title using base temperature provided in JSON
-    document.getElementById("baseTemp").innerHTML = "1753 - 2015 Base Temperature: " + baseTemp + "&#176; " + "C"
+function createMap() { 
 
     var w = 900
-    var h = 600
-    var padding = 75
+    var h = 650
 
-    currentWidth = parseInt(d3.select('body').style('width'), 10)
-
-    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    // console.log(data)
-
-    // create svg element to contain everything relate to the heat bars
-    var svg = d3.select("body")
-                .append("svg")
-                .classed("svg-content-responsive", true)
-                .attr("width", currentWidth)
-                .attr("height", h)
-
-    var valueX = data.map(x => x.year)
-    maxX = Math.max(...valueX)
-    minX = Math.min(...valueX)
-
-    const valueY = data.map(x => x.month)
-    maxY = Math.max(...valueY)
-    minY = Math.min(...valueY)
-
-    const xScale = d3.scaleLinear()
-    .domain([minX, maxX])
-    .range([0, currentWidth - padding*2])
-
-    const yScale = d3.scaleLinear()
-    .domain([maxY, 0])
-    .range([h, padding*2])
-
-    var barWidth = [(currentWidth - (padding*2)) / (valueX.length / 12)]
-    var barHeight = (h - (padding*2)) / 12
-
-    var tooltip = d3
-    .select('body')
+    var tooltip = d3.select('body')
     .append('div')
     .attr('class', 'tooltip')
     .attr('id', 'tooltip')
-    .style('opacity', 0);
+    .style("opacity", 1)
 
-    //create heat bars
-    svg.selectAll("rect")
-    .data(data)
+    var svg = d3.select("body")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h)
+
+    svg.selectAll("path")
+    .data(countyData)
     .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", (d, i) => ((Math.floor(i / 12) + 1) * barWidth) + padding)
-    .attr("y", (d, i) => ((i % 12) * barHeight) + padding)
-    .attr("width", barWidth)
-    .attr("height", barHeight)
+    .append("path")
     .attr("fill", (d) => {
-        if (d.variance <= -5)
-        return "#751CF1"
-        if (d.variance > -5 && d.variance <= -4)
-        return "#301CF1"
-        if (d.variance > -4 && d.variance <= -3)
-        return "#1C4AF1"
-        if (d.variance > -3 && d.variance <= -2)
-        return "#1C98F1" 
-        if (d.variance > -2 && d.variance <= -1)
-        return "#1CE9F1"
-        if (d.variance > -1 && d.variance <= 0)
-        return "#F1E71C"
-        if (d.variance > 0 && d.variance <= 1)
-        return "#F1AF1C"
-        if (d.variance > 1 && d.variance <= 2)
-        return "#F1801C"
-        if (d.variance > 2 && d.variance <= 3)
-        return "#F1411C"
-        if (d.variance > 3 && d.variance <= 4)
-        return "#581845"
-        else return "#c23700"
+        let educationLevel = educationData.find(element => element.fips === d.id).bachelorsOrHigher
+        if (educationLevel >= 40) {return colorData[0]}
+        if (educationLevel < 40 && educationLevel >= 30) {return colorData[1]}
+        if (educationLevel < 30 && educationLevel >= 20) {return colorData[2]}
+        if (educationLevel < 20 && educationLevel >= 15) {return colorData[3]}
+        if (educationLevel < 15 && educationLevel >= 10) {return colorData[4]}
+        if (educationLevel < 10 && educationLevel >= 10) {return colorData[5]}
+        if (educationLevel < 10) {return colorData[6]}
+
     })
+    .attr("d", d3.geoPath())
     .on("mouseover", function(event, d) {
-        // console.log(d)
-        tooltip.style("opacity", .75)
-        tooltip.html(d.year + " "
-        + "(" + month[d.month - 1] + ")" + "<br>" 
-        + "Temp:" + " " + (d.variance + 8.66).toFixed(2) + "&#176; " + "C" + " " + "<br>" 
-        + "Variance: " + d.variance)
-        tooltip.style("left", xScale(d.year) - currentWidth/2 + 170 + "px")
-        tooltip.style("top", yScale(d.month) - 725 + "px")
+        var windowWidth = window.innerWidth;
+        // console.log(windowWidth)
+        // console.log(d.id)
+        // console.log(educationData)
+        let found = educationData.find(element => element.fips === d.id);
+        // console.log(found)
+        tooltip.style("opacity", 1)
+        tooltip.html(found.area_name + ", " + found.state + " " + found.bachelorsOrHigher + "%")
+        tooltip.style("left", event.clientX - windowWidth/4 + 385 + "px")
+        tooltip.style("top", event.clientY - 30 + "px")
         tooltip.style("color", "black")
     })
     .on("mouseout", function(event, d) {
         tooltip.style("opacity", 0)
       })
+}    
 
-    // create x axis // tickFormat used for tick display along axis
-    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
-    svg.append("g")
-    .attr("id", "x-axis")
-    .attr("transform", "translate(75 , 525)")
-    .call(xAxis);
-
-    // create y axis
-    const yAxis = d3.axisLeft(yScale).tickFormat((d, i) => month[month.length - i])
-    svg.append("g")
-    .attr("id", "y-axis")
-    .attr("transform", "translate(75, -75)")
-    .call(yAxis);
-
+// convert json at URL to js object as topojson format 
+d3.json(countyURL).then((data, error) => {
+    if (error) {
+        console.log(log)
+    }
+    else {
+        // topojson.feature is a method to convert topojson format (more advanced)
+        // to geodata format (less advanced) so d3 can draw path from it
+        countyData = topojson.feature(data, data.objects.counties).features
+        // console.log(countyData)
+    }   
+    
+    d3.json(educationURL).then((data, error) => {
+        if (error) {
+            console.log(log)
+        }
+        else {
+            // topojson.feature is a method to convert topojson format (more advanced)
+            // to geodata format (less advanced) so d3 can draw path from it
+            educationData = data
+            // console.log(educationData)
+            createLegend() 
+            createMap() 
+        }   
+    })    
 }
+)
 
-// window.addEventListener('resize', createBar)
-// window.addEventListener('resize', function(event) {
-//     createBar(data, baseTemp)
-//     console.log("resize")
-// })
+function createLegend() { 
 
-window.onresize = function(){ location.reload(); }
+    let b0 = document.querySelector("#b0"); 
+    let b1 = document.querySelector("#b1"); 
+    let b2 = document.querySelector("#b2"); 
+    let b3 = document.querySelector("#b3"); 
+    let b4 = document.querySelector("#b4"); 
+    let b5 = document.querySelector("#b5"); 
+    let b6 = document.querySelector("#b6"); 
+      
+    b0.style.backgroundColor = colorData[6] 
+    b1.style.backgroundColor = colorData[5]
+    b2.style.backgroundColor = colorData[4]
+    b3.style.backgroundColor = colorData[3]
+    b4.style.backgroundColor = colorData[2]
+    b5.style.backgroundColor = colorData[1]
+    b6.style.backgroundColor = colorData[0]
+
+    // study this code below to make legend bars colors
+    // var x = d3.scaleLinear().domain([2.6, 50]).rangeRound([500, 760]);
+
+    // var color = d3
+    //     .scaleThreshold()
+    //     .domain(d3.range(0, 50, (50 - 2.6) / 6))
+    //     .range(d3.schemeBlues[7]);            
+
+    // var g = d3.select("body")
+    //     .append("svg")
+    //     .attr("width", 900)
+    //     .attr("height", 50)
+    //     .attr('transform', 'translate(0, 50)');
+
+    // g.selectAll('rect')
+    // .data(
+    //     color.range().map(function (d) {
+    //     d = color.invertExtent(d);
+    //     if (d[0] === null) {
+    //         d[0] = x.domain()[0];
+    //     }
+    //     if (d[1] === null) {
+    //         d[1] = x.domain()[1];
+    //     }
+    //     return d;
+    //     })
+    // )
+    // .enter()
+    // .append('rect')
+    // .attr('height', 10)
+    // .attr('x', function (d) {
+    //     return x(d[0]);
+    // })
+    // // .attr('y', 40)
+    // .attr('width', function (d) {
+    //     return x(d[1]) - x(d[0]);
+    // })
+    // .attr('fill', function (d) {
+    //     return color(d[0]);
+    // });
+    
+    // g.call(
+    // d3.axisBottom(x)
+    // .tickSize(13)
+    // .tickFormat(function (x) {
+    // return Math.round(x) + '%';
+    // })
+    // .tickValues(color.domain())
+    // )
+    // .select('.domain')
+    // .remove();  
+}
