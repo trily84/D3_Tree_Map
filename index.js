ca36ce5dcbaf6ed3c8a34110a22b5a60cc40c484
trily84 <tmly283@gmail.com>
2021-04-09 07:35:35 -0600

    first commit

--- index.js
+++ index.js
@@ -1,156 +1,160 @@
-let countyURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
-let educationURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
+// let countyURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+// let educationURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
 
-let countyData
-let educationData
+// let countyData
+// let educationData
 
-colorData = ["#053959", "#164c6e", "#356685", "#57819c", "#7b9aad", "#9cafba", "#a9bcc7"]
+// colorData = ["#053959", "#164c6e", "#356685", "#57819c", "#7b9aad", "#9cafba", "#a9bcc7"]
 
-function createMap() { 
+// function createMap() { 
+    
+// }
 
-    var w = 900
-    var h = 650
+let margin = {top: 10, right: 10, bottom: 10, left: 10},
+  width = 900
+  height = 600
 
-    var tooltip = d3.select('body')
+// append the svg object to the body of the page
+let svg = d3.select("#graph")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// read json data
+d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json", function(data) {
+
+  // Give the data to this cluster layout:
+  var root = d3.hierarchy(data).sum(function(d){return d.value}) // Here the size of each leave is given in the 'value' field in input data
+
+  // Then d3.treemap computes the position of each element of the hierarchy
+  d3.treemap()
+    .size([width, height])
+    (root)
+
+  var tooltip = d3
+    .select('body')
     .append('div')
     .attr('class', 'tooltip')
     .attr('id', 'tooltip')
-    .style("opacity", 1)
+    .style('opacity', 0);
 
-    var svg = d3.select("body")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h)
-
-    svg.selectAll("path")
-    .data(countyData)
+  // add rectangles:
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
     .enter()
-    .append("path")
-    .attr("fill", (d) => {
-        let educationLevel = educationData.find(element => element.fips === d.id).bachelorsOrHigher
-        if (educationLevel >= 40) {return colorData[0]}
-        if (educationLevel < 40 && educationLevel >= 30) {return colorData[1]}
-        if (educationLevel < 30 && educationLevel >= 20) {return colorData[2]}
-        if (educationLevel < 20 && educationLevel >= 15) {return colorData[3]}
-        if (educationLevel < 15 && educationLevel >= 10) {return colorData[4]}
-        if (educationLevel < 10 && educationLevel >= 10) {return colorData[5]}
-        if (educationLevel < 10) {return colorData[6]}
-
-    })
-    .attr("d", d3.geoPath())
-    .on("mouseover", function(event, d) {
-        var windowWidth = window.innerWidth;
-        // console.log(windowWidth)
-        // console.log(d.id)
-        // console.log(educationData)
-        let found = educationData.find(element => element.fips === d.id);
-        // console.log(found)
-        tooltip.style("opacity", 1)
-        tooltip.html(found.area_name + ", " + found.state + " " + found.bachelorsOrHigher + "%")
-        tooltip.style("left", event.clientX - windowWidth/4 + 385 + "px")
-        tooltip.style("top", event.clientY - 30 + "px")
+    .append("rect")
+      .attr('x', function(d) {return d.x0})
+      .attr('y', function(d) {return d.y0})
+      .attr('width', function(d) {return d.x1 - d.x0})
+      .attr('height', function(d) {return d.y1 - d.y0})
+      .style("fill", function(d) {
+        // console.log(d.data.category)  
+        var category = d.data.category
+        if (category == "2600") {
+            return "#ba776e"
+        }
+        if (category == "Wii") {
+            return "#9bafd4"
+        }
+        if (category == "NES") {
+            return "#c71053"
+        }
+        if (category == "GB") {
+            return "#9e8942"
+        }
+        if (category == "DS") {
+            return "#d18ea7"
+        }
+        if (category == "X360") {
+            return "#79549c"
+        }
+        if (category == "PS4") {
+            return "#a1b8bf"
+        }
+        if (category == "PS3") {
+            return "#769eab"
+        }
+        if (category == "PS2") {
+            return "#457b8c"
+        }
+        if (category == "PS") {
+            return "#1e6175"
+        }
+        if (category == "SNES") {
+            return "#6e6f85"
+        }
+        if (category == "GBA") {
+            return "#cacbde"
+        }
+        if (category == "3DS") {
+            return "#999177"
+        }
+        if (category == "N64") {
+            return "#756a45"
+        }
+        if (category == "XB") {
+            return "#c2a9d9"
+        }
+        if (category == "PC") {
+            return "#856211"
+        }
+        if (category == "PSP") {
+            return "#5f8769"
+        }
+        if (category == "XOne") {
+            return "#76de91"
+        }
+      })
+      .on("mouseover", function(event, d) {
+        // console.log(event.data.name)
+        tooltip.style("opacity", .8)
+        tooltip.html(event.data.name + "<br>" + " Category: " + event.data.category + "<br>" + " Value: " + event.data.value + " billions")
+        tooltip.style("left", d3.event.pageX + 40 + "px")
+        tooltip.style("top", d3.event.pageY + 10 + "px")
         tooltip.style("color", "black")
-    })
-    .on("mouseout", function(event, d) {
+      })
+      .on("mouseout", function(event, d) {
         tooltip.style("opacity", 0)
       })
-}    
 
-// convert json at URL to js object as topojson format 
-d3.json(countyURL).then((data, error) => {
-    if (error) {
-        console.log(log)
-    }
-    else {
-        // topojson.feature is a method to convert topojson format (more advanced)
-        // to geodata format (less advanced) so d3 can draw path from it
-        countyData = topojson.feature(data, data.objects.counties).features
-        // console.log(countyData)
-    }   
+  // add the text labels
+  let g = svg
+    .selectAll('g') 
+    .data(root.leaves())
+    .enter()
+    .append('g')
+    .attr('transform', d => 'translate(' + d.x0 + ',' + d.y0 + ')')
+  
+  g
+    .append('text')
+    .selectAll('tspan')
+    .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+    .enter()
+    .append('tspan')
+    .attr('x', 4)
+    .attr('y', (d, i) => 13 + i * 10)
+    .text(d => d)   
+
+
+  let legend = d3.selectAll("legend")
+    .append('g')
+    .selectAll('g')
+    .data(root.leaves())
+    .enter()
+    .append('g')
     
-    d3.json(educationURL).then((data, error) => {
-        if (error) {
-            console.log(log)
-        }
-        else {
-            // topojson.feature is a method to convert topojson format (more advanced)
-            // to geodata format (less advanced) so d3 can draw path from it
-            educationData = data
-            // console.log(educationData)
-            createLegend() 
-            createMap() 
-        }   
-    })    
-}
-)
+    legend  
+    .append("rect")
+        .attr('x', 500)
+        .attr('y', 500)
+        .attr('width', 200)
+        .attr('height', 200)
+        .style("fill", "black")
+ 
+})
 
-function createLegend() { 
 
-    let b0 = document.querySelector("#b0"); 
-    let b1 = document.querySelector("#b1"); 
-    let b2 = document.querySelector("#b2"); 
-    let b3 = document.querySelector("#b3"); 
-    let b4 = document.querySelector("#b4"); 
-    let b5 = document.querySelector("#b5"); 
-    let b6 = document.querySelector("#b6"); 
-      
-    b0.style.backgroundColor = colorData[6] 
-    b1.style.backgroundColor = colorData[5]
-    b2.style.backgroundColor = colorData[4]
-    b3.style.backgroundColor = colorData[3]
-    b4.style.backgroundColor = colorData[2]
-    b5.style.backgroundColor = colorData[1]
-    b6.style.backgroundColor = colorData[0]
-
-    // study this code below to make legend bars colors
-    // var x = d3.scaleLinear().domain([2.6, 50]).rangeRound([500, 760]);
-
-    // var color = d3
-    //     .scaleThreshold()
-    //     .domain(d3.range(0, 50, (50 - 2.6) / 6))
-    //     .range(d3.schemeBlues[7]);            
-
-    // var g = d3.select("body")
-    //     .append("svg")
-    //     .attr("width", 900)
-    //     .attr("height", 50)
-    //     .attr('transform', 'translate(0, 50)');
-
-    // g.selectAll('rect')
-    // .data(
-    //     color.range().map(function (d) {
-    //     d = color.invertExtent(d);
-    //     if (d[0] === null) {
-    //         d[0] = x.domain()[0];
-    //     }
-    //     if (d[1] === null) {
-    //         d[1] = x.domain()[1];
-    //     }
-    //     return d;
-    //     })
-    // )
-    // .enter()
-    // .append('rect')
-    // .attr('height', 10)
-    // .attr('x', function (d) {
-    //     return x(d[0]);
-    // })
-    // // .attr('y', 40)
-    // .attr('width', function (d) {
-    //     return x(d[1]) - x(d[0]);
-    // })
-    // .attr('fill', function (d) {
-    //     return color(d[0]);
-    // });
-    
-    // g.call(
-    // d3.axisBottom(x)
-    // .tickSize(13)
-    // .tickFormat(function (x) {
-    // return Math.round(x) + '%';
-    // })
-    // .tickValues(color.domain())
-    // )
-    // .select('.domain')
-    // .remove();  
-}
